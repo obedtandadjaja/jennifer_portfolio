207b5913a7b3926e3768eb00f035986b07b4a729
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-03-27 23:51:45 -0400

    update #9

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,5 @@
 class AttachmentsController < ApplicationController
-	# before_action :authenticate_user!, :except => [:show, :index]
+	before_action :authenticate_user!, :except => [:show, :index]
 
 	def index
 		@attachments = Attachment.all
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-	# before_action :authenticate_user!, :except => [:show, :index]
+	before_action :authenticate_user!, :except => [:show, :index]
 
 	def index
 		@projects = Project.all
